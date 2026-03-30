4757da27dd804c1aa5b8d6d847b98798ddd584c0
marjuremaiara <marjuremaiara@gmail.com>
2026-03-30 10:25:26 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "GAMA Carvões Premium",
+  description: "Carvão premium para churrasco e uso profissional",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
